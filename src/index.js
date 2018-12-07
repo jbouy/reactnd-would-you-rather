88e1b06a7af2915e58fb488eeb9a3bbd3d7c724d
James Bouy <jbouy@turner-industries.com>
2018-12-07 10:57:28 -0600

    feat(app): add <Provider> and store to the application

--- src/index.js
+++ src/index.js
@@ -3,5 +3,16 @@ import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 import App from './components/App';
+import {composeWithDevTools} from 'redux-devtools-extension';
+import {createStore} from 'redux';
+import reducer from './reducers';
+import {Provider} from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer, composeWithDevTools());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
