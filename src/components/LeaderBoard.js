6ed2223b84a03b639bd7989d8ad938d891e906e3
James Bouy <jbouy@turner-industries.com>
2018-12-11 12:34:18 -0600

    feat(leaderboard): add 'LeaderBoard' component

--- src/components/LeaderBoard.js
+++ src/components/LeaderBoard.js
@@ -1,5 +1,46 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {Grid, Segment, Header, Label} from 'semantic-ui-react';
+import {getUserLeaderBoard} from '../selectors/users';
+import AvatarContainer from './AvatarContainer';
 
-const LeaderBoard = props => <div>Leader Board</div>;
+class LeaderBoard extends Component {
+  render() {
+    const {leaderBoard} = this.props;
 
-export default LeaderBoard;
+    return (
+      <div>
+        {leaderBoard.map(l => (
+          <AvatarContainer key={l.id} header={l.name} avatar={l.avatarURL}>
+            <Grid>
+              <Grid.Row divided>
+                <Grid.Column width="10" verticalAlign="middle">
+                  <p>Answered questions: {l.answered}</p>
+                  <p>Created questions: {l.created}</p>
+                </Grid.Column>
+                <Grid.Column width="6" verticalAlign="middle">
+                  <Header textAlign="center" attached="top">
+                    Score
+                  </Header>
+                  <Segment attached textAlign="center">
+                    <Label circular color="teal" size="big">
+                      {l.score}
+                    </Label>
+                  </Segment>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          </AvatarContainer>
+        ))}
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    leaderBoard: getUserLeaderBoard(state),
+  };
+}
+
+export default connect(mapStateToProps)(LeaderBoard);
--- src/selectors/users.js
+++ src/selectors/users.js
@@ -1,3 +1,5 @@
+import _ from 'lodash';
+
 export function getAuthedUser({authedUser, users}) {
   return users[authedUser];
 }
@@ -8,4 +10,20 @@ export function getUser({users}, id) {
 
 export function getUsers({users}) {
   return users;
+}
+
+export function getUserLeaderBoard({users}) {
+  return _.chain(users)
+    .map(u => {
+      return {
+        id: u.id,
+        name: u.name,
+        avatarURL: u.avatarURL,
+        answered: _.keys(u.answers).length,
+        created: u.questions.length,
+        score: _.keys(u.answers).length + u.questions.length,
+      };
+    })
+    .sortBy(u => -u.score)
+    .value();
 }
